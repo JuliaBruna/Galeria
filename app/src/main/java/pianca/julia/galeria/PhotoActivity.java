c6d2933e6372a858741e73f13aee28d4a1103589
bruna e julia <brujure2023@gmail.com>
2024-05-21 14:42:54 -0300

    Concertar alguns erros

---
+++ app/src/main/java/pianca/julia/galeria/PhotoActivity.java
@@ -0,0 +1,18 @@
+package pianca.julia.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto); // obtem o elemento tbPhoto
+        setSupportActionBar(toolbar); // indica para PhotoActivity que tbPhoto deve ser considerado como a ActionBar padrão da tela
+    }
+}
