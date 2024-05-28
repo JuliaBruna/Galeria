879d7cbd9a9fb27e3f8a9236a70020e78e437d00
bruna e julia <brujure2023@gmail.com>
2024-05-28 13:33:42 -0300

    Continuar a concertar alguns erros

--- app/src/main/java/pianca/julia/galeria/MainActivity.java
+++ app/src/main/java/pianca/julia/galeria/MainActivity.java
@@ -1,11 +1,33 @@
 package pianca.julia.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opCamera:
+                dispatchTakePictureIntent(); // Caso o ícone de câmera tenha sido clicado, então será executado código que dispara a câmera do celular
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater(); // cria um “inflador de menu”
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/pianca/julia/galeria/PhotoActivity.java
+++ app/src/main/java/pianca/julia/galeria/PhotoActivity.java
@@ -1,12 +1,34 @@
 package pianca.julia.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto(); // Caso o ícone de câmera tenha sido clicado, então será executado código que compartilha a foto
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
